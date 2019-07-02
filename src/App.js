9c9c6a1ea844689758b4935aed97897542674d81
k-westbrook <kcheriewestbrook@gmail.com>
2019-07-01 23:25:35 -0700

    initial game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state =
+      {
+        board: [0, 1, 0,
+          0, 0, 0,
+          0, 0, 0]
+      }
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle(evt) {
+
+    let index = evt.target.getAttribute('value');
+    console.log(evt.target.value);
+    evt.persist();
+    navigator.testValue = evt;
+
+    let newBoard = this.state.board.slice();
+
+
+    if (newBoard[index] === 0) {
+      newBoard[index] = 1;
+    } else {
+      newBoard[index] = 0;
+    }
+
+    this.setState({ board: newBoard })
+
+
+  }
+  render() {
+    console.log(this.state)
+    return (
+      <div className="board" >
+        {this.state.board.map((tile, index) => {
+
+          return (
+            (tile === 0) ? <div key={index} value={index} className='red' name='tile' onClick={this.toggle}></div>
+              :
+              <div key={index} value={index} name='tile' className='blue' onClick={this.toggle}></div>
+
+          )
+        }
+        )
+        }
+
+      </div>
+    );
+  }
 }
 
 export default App;
